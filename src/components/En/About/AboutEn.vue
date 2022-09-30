13bf1e463bb2b9454481d14877035655c422a822
Salem-Tarek <salemtarek224@gmail.com>
2022-10-01 01:29:39 +0200

    Adding real Data to About Page

--- src/components/En/About/AboutEn.vue
+++ src/components/En/About/AboutEn.vue
@@ -5,7 +5,7 @@
             <v-container style="z-index: 1">
                 <div class="content white--text">
                     <p class="text-uppercase display-1 font-weight-bold">About Us</p>
-                    <p class="text-subtitle-2 text-sm-h6">"Lorem ipsum dolor sit amet consectetur, adipisicing elit"</p>
+                    <p class="text-subtitle-2 text-sm-h6">"One of the leading companies in the field of security systems, finishes and firefighting systems"</p>
                 </div>
             </v-container>
         </div>
@@ -18,7 +18,7 @@
                     <v-col cols="12" sm="6" class="mx-auto text-center px-6">
                         <h1 class="text-uppercase">"Home Security Experts"</h1>
                         <p class="subtitle-2 mb-0">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi obcaecati hic iusto quia deleniti unde quam placeat maxime autem ex est earum repellendus id, neque magni sunt voluptatibus similique architecto.
+                            The engineering company is distinguished by its diversity in providing the fields of security systems with superiority in the performance of each system, through the multiplicity in the system of the specialized technical department for each system.
                         </p>
                     </v-col>
                 </v-row>
@@ -27,13 +27,13 @@
             <v-row class="my-5">
                 <v-col cols="12" md="5">
                     <p class="mb-0 title font-weight-bold grey--text text--darken-3">Some words about us</p>
-                    <p class="subtitle-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum aspernatur laudantium, repellendus rerum eius quia. Repellat odit recusandae exercitationem amet.</p>
-                    <p class="subtitle-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum aspernatur laudantium, repellendus rerum eius quia. Repellat odit recusandae exercitationem amet.</p>
+                    <p class="subtitle-2">One of the leading companies in the field of security systems and has carried out many projects with major companies in the private sector, the public sector, and government agencies and agencies with the highest efficiency and effectiveness.</p>
+                    <p class="subtitle-2">The engineering company is distinguished in the speed of responding to sudden malfunctions through a technical work team at the highest level.. by receiving information about sudden malfunctions and moving to the customer within 24 hours to address those malfunctions.</p>
+                    <p class="subtitle-2">The engineering company is distinguished by providing after-sales service, which is the period during the warranty through making periodic maintenance reports, good follow-up to the various systems, and activating annual maintenance contracts after the end of the warranty period to follow up the operation of the various systems.</p>
+
                     <p class="mb-0 title font-weight-bold grey--text text--darken-3">Mission</p>
-                    <p class="subtitle-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum aspernatur laudantium, repellendus rerum eius quia. Repellat odit recusandae exercitationem amet.</p>
-                    <p class="subtitle-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum aspernatur laudantium, repellendus rerum eius quia. Repellat odit recusandae exercitationem amet.</p>
-                    <p class="mb-0 title font-weight-bold grey--text text--darken-3">Filosofy</p>
-                    <p class="subtitle-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum aspernatur laudantium, repellendus rerum eius quia. Repellat odit recusandae exercitationem amet.</p>
+                    <p class="subtitle-2">The engineering company is distinguished by its diversity in providing the fields of security systems with superiority in the performance of each system, through the multiplicity in the system of the specialized technical department for each system.</p>
+                    <p class="subtitle-2">The engineering company is distinguished by the presence of a highly trained team during the installation and periodic maintenance works. This team is distinguished by the order and accuracy of the monthly periodic reports.</p>
                 </v-col>
                 <v-col cols="12" md="7">
                     <div class="feat d-flex align-start grey lighten-3 pa-5 my-5" style="border: 1px solid #333!important;">
@@ -41,7 +41,7 @@
                         <div class="content">
                             <p class="mb-0 title font-weight-bold">Professional Service</p>
                             <p class="subtitle-2 mb-0 grey--text text--darken-4">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi obcaecati hic iusto quia deleniti unde quam placeat maxime autem ex est earum repellendus id, neque magni sunt voluptatibus similique architecto.
+                                The engineering company is distinguished by the presence of a highly trained team during the installation and periodic maintenance works. This team is distinguished by the order and accuracy of the monthly periodic reports.
                             </p>
                         </div>
                     </div>
@@ -50,7 +50,7 @@
                         <div class="content">
                             <p class="mb-0 title font-weight-bold">Affordable Prices</p>
                             <p class="subtitle-2 mb-0 grey--text text--darken-4">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi obcaecati hic iusto quia deleniti unde quam placeat maxime autem ex est earum repellendus id, neque magni sunt voluptatibus similique architecto.
+                                The engineering company is distinguished by its great prices, excellent quality and support available 24 hours a day through a highly trained team to detect and repair malfunctions.
                             </p>
                         </div>
                     </div>
@@ -59,7 +59,7 @@
                         <div class="content">
                             <p class="mb-0 title font-weight-bold">Great Support</p>
                             <p class="subtitle-2 mb-0 grey--text text--darken-4">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi obcaecati hic iusto quia deleniti unde quam placeat maxime autem ex est earum repellendus id, neque magni sunt voluptatibus similique architecto.
+                                The engineering company is distinguished in the speed of responding to sudden malfunctions through a technical work team at the highest level.. by receiving information about sudden malfunctions and moving to the customer within 24 hours to address those malfunctions.
                             </p>
                         </div>
                     </div>
@@ -68,7 +68,7 @@
                         <div class="content">
                             <p class="mb-0 title font-weight-bold">Service Warranty</p>
                             <p class="subtitle-2 mb-0 grey--text text--darken-4">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi obcaecati hic iusto quia deleniti unde quam placeat maxime autem ex est earum repellendus id, neque magni sunt voluptatibus similique architecto.
+                                The engineering company is distinguished by providing after-sales service, which is the period during the warranty through making periodic maintenance reports, good follow-up to the various systems, and activating annual maintenance contracts after the end of the warranty period to follow up the operation of the various systems.
                             </p>
                         </div>
                     </div>
