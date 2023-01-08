a1e015770651816859edb5c4e8cbef65a4fe866c
Salem-Tarek <salemtarek224@gmail.com>
2023-01-09 01:38:06 +0200

    Deploying Dist Folder

---
+++ precache-manifest.a2d70cc62c234c90458f30fe50b5a49a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9f1893a47bb9f781bf",
+    "url": "css/app.257d2253.css"
+  },
+  {
+    "revision": "61de30d881d81ad72631",
+    "url": "css/chunk-vendors.07bf17fb.css"
+  },
+  {
+    "revision": "bc7ebab61a07940fff3e8f070fb4af20",
+    "url": "img/bg_parallax.bc7ebab6.jpg"
+  },
+  {
+    "revision": "48e2cceedf47b274ed5fc963822124d6",
+    "url": "img/logo.48e2ccee.png"
+  },
+  {
+    "revision": "0d4aa5fcbcd58164e213359c4de0945e",
+    "url": "img/prod-1.0d4aa5fc.jpg"
+  },
+  {
+    "revision": "6eac6e8dc5e78415ebf65eda21f3740a",
+    "url": "img/service1.6eac6e8d.jpg"
+  },
+  {
+    "revision": "20dd45d4ad09857a1a8df980dc20a297",
+    "url": "img/service2.20dd45d4.jpeg"
+  },
+  {
+    "revision": "10ca4093011dd6dce45dfa57ae349a59",
+    "url": "img/service3.10ca4093.jpeg"
+  },
+  {
+    "revision": "8a228b4148866ea7be59c72ccc26d82e",
+    "url": "index.html"
+  },
+  {
+    "revision": "cd9f1893a47bb9f781bf",
+    "url": "js/app.6bd39c61.js"
+  },
+  {
+    "revision": "61de30d881d81ad72631",
+    "url": "js/chunk-vendors.1f4cc788.js"
+  },
+  {
+    "revision": "848b7f1c508dd034399a253acb6ad8e2",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
