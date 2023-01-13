69d09a4c62d6a3673aed082b45cc6ba45b89b0bb
Salem-Tarek <salemtarek224@gmail.com>
2023-01-13 03:51:26 +0200

    Removing Arabic compoenents

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,17 +1,15 @@
 <template>
-    <ProductPageEn v-if="getLang === 'En'" />
-    <ProductPageAr v-else/>
+    <ProductPageEn />
+    <!-- <ProductPageAr v-else/> -->
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import ProductPageEn from '../components/En/ProductPage/ProductPageEn.vue'
-import ProductPageAr from '../components/Ar/ProductPage/ProductPageAr.vue'
 export default {
     name: "ProductPage",
     components:{
         ProductPageEn,
-        ProductPageAr,
     },
     computed:{
         ...mapGetters(['getLang'])
